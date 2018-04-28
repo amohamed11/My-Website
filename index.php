702c360eefdb84967e5c73ace526f710988a050f
Anas Mohamed <asxc300@gmail.com>
2018-04-27 20:29:28 -0600

    HTML/CSS base
    Created the HTML/CSS base for the website. Included in is bootstrap.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    <!-- Custom style -->
+    <link rel="stylesheet" href="css/page.css">
+
+  </head>
+  <body>
+    <div class="container">
+      <div class="page-header">
+        <h1 class="title">Anas Mohamed</h1>
+        <p class="info"> This is my personal website, dedicated to the one and only. Me!</p>
+      </div>
+    </div>
+
+
+  </body>
+</html>
