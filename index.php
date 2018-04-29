3457218d0201f486bb7f5d624ccdd8e9bdb3e044
Anas Mohamed <asxc300@gmail.com>
2018-04-29 15:07:40 -0600

    Updated the Base Template
    Added some information, and updated the look. Next Plan is to add images and Experiences.

--- index.php
+++ index.php
@@ -1,26 +1,39 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-
-    <!-- Custom style -->
-    <link rel="stylesheet" href="css/page.css">
-
+    <?php include('Meta_Links.html'); ?>
   </head>
   <body>
-    <div class="container">
-      <div class="page-header">
-        <h1 class="title">Anas Mohamed</h1>
-        <p class="info"> This is my personal website, dedicated to the one and only. Me!</p>
+    <div class="site-warapper">
+      <div class="site-warapper-inner">
+        <div class="head-container">
+          <div class="page-header">
+            <h1 class="title">Anas Mohamed</h1>
+            <p class="info"> This is my personal website, dedicated to the one and only. Me!</p>
+          </div>
+        </div>
+        <!--- End of Container div-->
+        <div class="inner-cover">
+          <h1 class="cover-heading">About Me. </h1>
+          <p class="lead">I am currently a second year Computing Science student at University of Alberta.<br/> 
+            My interests include:</p>
+            <ul class="list">
+              <li>Software Development</li>
+              <li>Web-Design </li>
+              <li>App Development (Android)</li>
+            </ul> 
+          <p2 class= "lead"> I am also interested in Video Game Development, but mostly as a side interest.</p2>
+        </div>
+        <!--- End of Inner-Cover div-->
+        <div class="footer">
+            <div class="inner">
+              <p>Feel free to contact me using my github: <a href="github.com/amohamed11">@amohamed11</a>.</p>
+            </div>
+        </div>
+        <!--- End of Footer div-->
       </div>
+        <!--- End of Site-Wrapper-Inner div-->
     </div>
-
 
   </body>
 </html>
